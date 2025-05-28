10eb27dbceff2bf3b0ab5faee9635fa2c33aaeaf
ThiagoHenrique04 <thiagohngomes04@gmail.com>
2025-05-28 11:24:55 -0300

    User registration tests added

---
+++ app/src/test/java/pages/RegisterPage.java
@@ -0,0 +1,62 @@
+package pages;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+public class RegisterPage {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    public RegisterPage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+    
+    // ========== Locators ==========
+    private By AccessRegisterButton = By.cssSelector("button[class='style__ContainerButton-sc-1wsixal-0 ihdmxA button__child']");
+    private By Emailinput = By.cssSelector("form[class='styles__ContainerFormRegister-sc-7fhc7g-0 keVbpY'] input[placeholder='Informe seu e-mail']");
+    private By Nameinput = By.name("name");
+    private By Passwordinput = By.cssSelector("form[class='styles__ContainerFormRegister-sc-7fhc7g-0 keVbpY'] input[placeholder='Informe sua senha']");
+    private By passwordConfirmationinput = By.name("passwordConfirmation");
+    private By RegisterButton = By.cssSelector("button[class='style__ContainerButton-sc-1wsixal-0 CMabB button__child']");
+    private By saldoToggle = By.cssSelector("label.styles__Container-sc-1pngcbh-0.kIwoPV");
+    private By MsgModal = By.id("modalText");
+
+
+
+    // ========== Actions ==========
+    public void clickAccessRegisterButton() {
+        wait.until(ExpectedConditions.elementToBeClickable(AccessRegisterButton)).click();
+    }
+
+    public void enterEmail(String email) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(Emailinput)).sendKeys(email);
+    }
+
+    public void enterName(String name) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(Nameinput)).sendKeys(name);
+    }
+
+    public void enterPassword(String password) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(Passwordinput)).sendKeys(password);
+    }
+
+    public void enterPasswordConfirmation(String passwordConfirmation) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(passwordConfirmationinput)).sendKeys(passwordConfirmation);
+    }
+
+    public void createAccountWithSaldo() {
+         wait.until(ExpectedConditions.elementToBeClickable(saldoToggle)).click();
+
+    }
+
+    public void clickRegisterButton() {
+        wait.until(ExpectedConditions.elementToBeClickable(RegisterButton)).click();
+    }
+
+    public String getMessage() {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(MsgModal)).getText();
+    }
+}
--- app/src/test/java/tests/RegisterTest.java
+++ app/src/test/java/tests/RegisterTest.java
@@ -1,14 +1,137 @@
 package tests;
 
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
+
+import com.github.javafaker.Faker;
 
 import base.BaseTest;
+import io.qameta.allure.*;
+import pages.RegisterPage;
 
+@Epic("Register Tests")
+@Feature("User Registration")
 public class RegisterTest extends BaseTest{
     
-    @Test
-    public void testePiloto() {
-        String title = driver.getTitle();
-        System.out.println("Title of the page is: " + title);
+    private final Faker faker = new Faker();
+    
+
+    @Test(description = "CT-001 - Test Register user successfully - without balance")
+    @Severity(SeverityLevel.BLOCKER)
+    public void registerUserSuccessfullyWhithoutBalance() {
+
+        String generatedPassword = faker.internet().password(8, 16, true, true, true);
+
+        RegisterPage registerPage = new RegisterPage(driver);
+        registerPage.clickAccessRegisterButton();
+        registerPage.enterEmail(faker.internet().emailAddress());
+        registerPage.enterName(faker.name().fullName());
+        registerPage.enterPassword(generatedPassword);
+        registerPage.enterPasswordConfirmation(generatedPassword);
+        registerPage.clickRegisterButton();
+        registerPage.getMessage()
+            .contains("criada com sucesso");
+        //Depois realizar a validação do saldo
+
+    }
+
+    @Test(description = "CT-002 - Test Register user successfully - without balance")
+    @Severity(SeverityLevel.BLOCKER)
+    public void registerUserSuccessfullyWhithBalance() {
+
+        String generatedPassword = faker.internet().password(8, 16, true, true, true);
+
+        RegisterPage registerPage = new RegisterPage(driver);
+        registerPage.clickAccessRegisterButton();
+        registerPage.enterEmail(faker.internet().emailAddress());
+        registerPage.enterName(faker.name().fullName());
+        registerPage.enterPassword(generatedPassword);
+        registerPage.enterPasswordConfirmation(generatedPassword);
+        registerPage.createAccountWithSaldo();
+        registerPage.clickRegisterButton();
+        registerPage.getMessage()
+            .contains("criada com sucesso");
+        //Depois realizar a validação do saldo
+    }
+
+    @Test(description = "CT-003 - Test Register User Without Filling in Name")
+    @Severity(SeverityLevel.BLOCKER)
+    public void registerUserWithoutFillingName() {
+
+        String generatedPassword = faker.internet().password(8, 16, true, true, true);
+
+        RegisterPage registerPage = new RegisterPage(driver);
+        registerPage.clickAccessRegisterButton();
+        registerPage.enterEmail(faker.internet().emailAddress());
+        registerPage.enterPassword(generatedPassword);
+        registerPage.enterPasswordConfirmation(generatedPassword);
+        registerPage.clickRegisterButton();
+        registerPage.getMessage()
+            .contains("Nome não pode ser vazio.");
+    }
+
+
+    @Test(description = "CT-004 - Test Register User Without Filling in Email")
+    @Severity(SeverityLevel.BLOCKER)
+    public void registerUserWithoutFillingEmail() {
+
+        String generatedPassword = faker.internet().password(8, 16, true, true, true);
+
+        RegisterPage registerPage = new RegisterPage(driver);
+        registerPage.clickAccessRegisterButton();
+        registerPage.enterName(faker.name().fullName());
+        registerPage.enterPassword(generatedPassword);
+        registerPage.enterPasswordConfirmation(generatedPassword);
+        registerPage.clickRegisterButton();
+        registerPage.getMessage()
+            .contains("Email não pode ser vazio.");
+    }
+
+    @Test(description = "CT-005 - Test Register User Without Filling in Password")
+    @Severity(SeverityLevel.BLOCKER)
+    public void registerUserWithoutFillingPassword() {
+
+        String generatedPassword = faker.internet().password(8, 16, true, true, true);
+
+        RegisterPage registerPage = new RegisterPage(driver);
+        registerPage.clickAccessRegisterButton();
+        registerPage.enterEmail(faker.internet().emailAddress());
+        registerPage.enterName(faker.name().fullName());
+        registerPage.enterPasswordConfirmation(generatedPassword);
+        registerPage.clickRegisterButton();
+        registerPage.getMessage()
+            .contains("Senha não pode ser vazio.");
+    }
+
+    @Test(description = "CT-006 - Test Register User Without Filling in Password Confirmation")
+    @Severity(SeverityLevel.BLOCKER)
+    public void registerUserWithoutFillingPasswordConfirmation() {
+
+        String generatedPassword = faker.internet().password(8, 16, true, true, true);
+
+        RegisterPage registerPage = new RegisterPage(driver);
+        registerPage.clickAccessRegisterButton();
+        registerPage.enterEmail(faker.internet().emailAddress());
+        registerPage.enterName(faker.name().fullName());
+        registerPage.enterPassword(generatedPassword);
+        registerPage.clickRegisterButton();
+        registerPage.getMessage()
+            .contains("criada com sucesso");
+    }
+
+    @Test(description = "CT-007 - Test Register User with different password and password confirmation")
+    @Severity(SeverityLevel.BLOCKER)
+    public void RegisterUserWithDifferentPasswordAndPasswordConfirmation() {
+
+        String generatedPassword = faker.internet().password(8, 16, true, true, true);
+
+        RegisterPage registerPage = new RegisterPage(driver);
+        registerPage.clickAccessRegisterButton();
+        registerPage.enterEmail(faker.internet().emailAddress());
+        registerPage.enterName(faker.name().fullName());
+        registerPage.enterPassword(generatedPassword);
+        registerPage.enterPasswordConfirmation("teste123");
+        registerPage.clickRegisterButton();
+        registerPage.getMessage()
+            .contains("As senhas não são iguais.");
     }
 }
